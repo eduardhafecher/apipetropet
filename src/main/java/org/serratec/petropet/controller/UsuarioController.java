815afdbdfbb98baf348c455db91f844dab99eefd
eduardhafecher <eduardha.alves@gmail.com>
2025-06-29 14:00:01 -0300

    classe UsuarioController criada

---
+++ src/main/java/org/serratec/petropet/controller/UsuarioController.java
@@ -0,0 +1,62 @@
+package org.serratec.petropet.controller;
+
+import jakarta.validation.Valid;
+import org.serratec.petropet.dto.UsuarioRequestDTO;
+import org.serratec.petropet.dto.UsuarioResponseDTO;
+import org.serratec.petropet.exceptions.EmailException;
+import org.serratec.petropet.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponseDTO> criarUsuario(@Valid @RequestBody UsuarioRequestDTO usuarioRequestDTO) {
+        try {
+            UsuarioResponseDTO novoUsuario = usuarioService.criarUsuario(usuarioRequestDTO);
+            return new ResponseEntity<>(novoUsuario, HttpStatus.CREATED);
+        } catch (EmailException e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> buscarUsuarioPorId(@PathVariable Long id) {
+        UsuarioResponseDTO usuario = usuarioService.buscarPorId(id);
+        return new ResponseEntity<>(usuario, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> atualizarUsuario(@PathVariable Long id, @Valid @RequestBody UsuarioRequestDTO usuarioRequestDTO) {
+        try {
+            UsuarioResponseDTO usuarioAtualizado = usuarioService.atualizarUsuario(id, usuarioRequestDTO);
+            return new ResponseEntity<>(usuarioAtualizado, HttpStatus.OK);
+        } catch (EmailException e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT); 
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+        usuarioService.deletarUsuario(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDTO>> listarTodosUsuarios() {
+        List<UsuarioResponseDTO> usuarios = usuarioService.listarTodosUsuarios();
+        if (usuarios.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(usuarios, HttpStatus.OK);
+    }
+}
