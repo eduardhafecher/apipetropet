eb73c73ea4ae84ddaacbffa681f25336e1bda7cc
eduardhafecher <eduardha.alves@gmail.com>
2025-06-29 15:39:16 -0300

    configurando o application properties

---
+++ src/main/java/org/serratec/petropet/config/SecurityDevConfig.java
@@ -0,0 +1,25 @@
+package org.serratec.petropet.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Profile("dev") // essa configuração só será ativada no perfil dev
+public class SecurityDevConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions().disable());
+
+        return http.build();
+    }
+}
